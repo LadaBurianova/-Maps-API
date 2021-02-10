4b661ce43a3cf1d05e8ed8021bc17866c1c7cc06
bakamouto <bakamouto@gmail.com>
2021-02-10 19:04:52 +0300

    scale editor

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ while run:
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             run = False
+        elif ev.type == pygame.KEYDOWN:
+            if ev.mod == pygame.K_PAGEDOWN:
+                params['z'] += 1
+            elif ev.mod == pygame.K_PAGEUP:
+                params['z'] -= 1
+            elif ev.key == pygame.K_UP:
+                pass
     screen.fill((0, 0, 0))
     name = get_image()
     image = pygame.image.load(name)
