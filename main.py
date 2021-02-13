4e5cfa694cd35254bb752b8bb260ebaaf75d2c3a
LadaBurianova <bulanova.dasha2004@yandex.ru>
2021-02-13 18:21:07 +0300

    text_editor.v.0 ready

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.init()
 COLOR_ACTIVE = (255, 204, 0)
 COLOR_INACTIVE = (58, 117, 196)
 INPUT_FONT = pygame.font.Font(None, 32)
+API_SERVER = 'https://static-maps.yandex.ru/1.x/?'
 
 
 def change_color(box, ev, active):
@@ -18,9 +19,7 @@ def change_color(box, ev, active):
     return active, color
 
 
-def get_image():
-    api_server = 'https://static-maps.yandex.ru/1.x/?'
-    response = requests.get(api_server, params)
+def get_image(response):
     if not response:
         print("Http статус:", response.status_code, "(", response.text, ")")
         sys.exit(response.status_code)
@@ -42,6 +41,8 @@ input_box = pygame.Rect(10, 410, 150, 32)
 text = ''
 color = COLOR_INACTIVE
 active = False
+response = requests.get(API_SERVER, params)
+image = get_image(response)
 
 while run:
     for ev in pygame.event.get():
@@ -53,23 +54,25 @@ while run:
                     if ev.key == pygame.K_RETURN:
                         if text != '':
                             try:
-                                print('ok')
-                            except FileNotFoundError:
-                                pass
-                        else:
-                            pass
+                                response = requests.get(text)
+                                image = get_image(response)
+                            except Exception:
+                                response = requests.get(API_SERVER, params)
                     elif ev.key == pygame.K_BACKSPACE:
                         text = text[:-1]
                     else:
                         text += ev.unicode
             if ev.key == pygame.K_PAGEDOWN:
                 params['z'] = str(min(13, int(params['z']) + 1))
+                response = requests.get(API_SERVER, params)
+                image = get_image(response)
             elif ev.key == pygame.K_PAGEUP:
                 params['z'] = str(max(0, int(params['z']) - 1))
+                response = requests.get(API_SERVER, params)
+                image = get_image(response)
         elif ev.type == pygame.MOUSEBUTTONDOWN:
             active, color = change_color(input_box, ev, active)
     screen.fill((0, 0, 0))
-    image = get_image()
     screen.blit(image, (10, 10))
     txt_surface = INPUT_FONT.render(text, True, color)
     input_box.w = max(200, txt_surface.get_width() + 10)
